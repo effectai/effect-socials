94e1efc0a11c44d875fd3df365e58e1d60f2c637
djmbritt <david@effect.ai>
2022-07-22 11:15:02 +0200

    Fix font-awesome icons

--- pages/start.vue
+++ pages/start.vue
@@ -8,22 +8,22 @@
 
             <div class="buttons is-centered px-6 mx-6">
               <button class="button is-link is-light mt-3 is-fullwidth is-large" @click.prevent="type='like'; nextStep()">
-                <span><font-awesome-icon icon="fa-solid fa-heart" /></span>
+                <span><font-awesome-icon class="mx-2 is-small" icon="fa-solid fa-heart" /></span>
                 <span>&nbsp;Likes</span>
               </button>
               <br><br><br>
               <button class="button is-link is-light mt-3 is-fullwidth is-large has-tooltip-arrow has-tooltip-top has-tooltip-info" data-tooltip="Coming Soon!" @click.prevent="type='follow'; nextStep()" disabled>
-                <span><font-awesome-icon icon="fa-solid fa-user-group" /></span>
+                <span><font-awesome-icon class="mx-2 is-small" icon="fa-solid fa-user-group" /></span>
                 <span>&nbsp;Follows</span>
               </button>
               <br><br><br>
               <button class="button is-link is-light mt-3 is-fullwidth is-large has-tooltip-arrow has-tooltip-top has-tooltip-info" data-tooltip="Coming Soon!" @click.prevent="type='retweet'; nextStep()" disabled>
-                <span><font-awesome-icon icon="fa-solid fa-retweet" /></span>
+                <span><font-awesome-icon class="mx-2 is-small" icon="fa-solid fa-retweet" /></span>
                 <span>&nbsp;Retweets</span>
               </button>
               <br><br><br>
               <button class="button is-link is-light mt-3 is-fullwidth is-large has-tooltip-arrow has-tooltip-top has-tooltip-info" data-tooltip="Coming Soon!" @click.prevent="type='reply'; nextStep()" disabled>
-                <span><font-awesome-icon icon="fa-solid fa-reply" /></span>
+                <span><font-awesome-icon class="mx-2 is-small" icon="fa-solid fa-reply" /></span>
                 <span>&nbsp;Replies</span>
               </button>
             </div>
