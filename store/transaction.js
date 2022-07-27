5e4663ac1635039f0ca604e91683a53cc122e7d9
djmbritt <david@effect.ai>
2022-07-27 12:49:52 +0200

    Add is isEmpty and removeTx methods

--- store/transaction.js
+++ store/transaction.js
@@ -10,20 +10,34 @@ export default {
         if (transaction) {
             state.transactions.list.push(transaction)
         }
+      },
+      REMOVE_TRANSACTION (state, transaction) {        
+        if (state.transactions.list) {
+            state.transactions.list.splice(state.transactions.list.indexOf(transaction), 1)
+        }
       }
     },
     getters: {
       transactionById (state) {
         return id => state.transactions ? state.transactions.find(c => c.id === id) : null
       },
-      transactionsByUser (state) {
-        return userId => state.transactions ? state.transactions[userId] : null
+      allTransactions (state) {
+        return state.transactions ? state.transactions.list : []
+      },
+      transactionsIsEmpty (state) {
+        return state.transactions?.list ? state.transactions?.list.length === 0 : true
       }
+
     },
     actions: {
       addTransaction ({ commit }, transaction) {
         if (transaction) {
           commit('ADD_TRANSACTION', transaction)
+        }
+      },
+      removeTransaction ({ commit }, transaction) {
+        if (transaction) {
+          commit('REMOVE_TRANSACTION', transaction)
         }
       }
     },
