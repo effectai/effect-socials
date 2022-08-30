f79402c6e0d2e2ea7370c7130df022ecc078f1fb
djmbritt <david@effect.ai>
2022-08-30 15:48:39 +0200

    Add insta and follow

--- pages/start.vue
+++ pages/start.vue
@@ -9,22 +9,27 @@
             <div class="buttons is-centered px-6 mx-6">
               <button class="button is-link is-light mt-3 is-fullwidth is-large" @click.prevent="type='like'; nextStep()">
                 <span><font-awesome-icon class="mx-2 icon is-small" icon="fa-solid fa-heart" /></span>
-                <span>&nbsp;Likes</span>
+                <span>&nbsp;Twitter Likes</span>
               </button>
               <br><br><br>
               <button class="button is-link is-light mt-3 is-fullwidth is-large has-tooltip-arrow has-tooltip-top has-tooltip-info" @click.prevent="type='retweet'; nextStep()">
                 <span><font-awesome-icon class="mx-2 icon is-small" icon="fa-solid fa-user-group" /></span>
-                <span>&nbsp;Retweets</span>
+                <span>&nbsp;Twitter Retweets</span>
               </button>
               <br><br><br>
               <button class="button is-link is-light mt-3 is-fullwidth is-large has-tooltip-arrow has-tooltip-top has-tooltip-info" data-tooltip="Coming Soon!" @click.prevent="type='follow'; nextStep()" disabled>
                 <span><font-awesome-icon class="mx-2 icon is-small" icon="fa-solid fa-retweet" /></span>
-                <span>&nbsp;Follows</span>
+                <span>&nbsp;Twitter Follows</span>
               </button>
               <br><br><br>
               <button class="button is-link is-light mt-3 is-fullwidth is-large has-tooltip-arrow has-tooltip-top has-tooltip-info" data-tooltip="Coming Soon!" @click.prevent="type='reply'; nextStep()" disabled>
                 <span><font-awesome-icon class="mx-2 icon is-small" icon="fa-solid fa-reply" /></span>
-                <span>&nbsp;Replies</span>
+                <span>&nbsp;Twitter Replies</span>
+              </button>              
+              <br><br><br>
+              <button class="button is-link is-light mt-3 is-fullwidth is-large has-tooltip-arrow has-tooltip-top has-tooltip-info" data-tooltip="Coming Soon!" @click.prevent="type='hearts'; nextStep()" disabled>
+                <span><font-awesome-icon class="mx-2 icon is-small" icon="fa-solid fa-heart" /></span>
+                <span>&nbsp;Instagram Likes</span>
               </button>
             </div>
           </div>
@@ -113,6 +118,7 @@ export default {
           this.campaigns.like = await this.effectsdk.force.getCampaign(process.env.NUXT_ENV_CAMPAIGN_LIKE_ID)
           // this.campaigns.reply = await this.effectsdk.force.getCampaign(process.env.NUXT_ENV_CAMPAIGN_REPLY_ID)
           this.campaigns.follow = await this.effectsdk.force.getCampaign(process.env.NUXT_ENV_CAMPAIGN_FOLLOW_ID)
+          this.campaigns.instagram = await this.effectsdk.force.getCampaign(process.env.NUXT_ENV_CAMPAIGN_INSTAGRAM_ID)
           
         } catch (error) {
           this.setErrorMessage(error)
